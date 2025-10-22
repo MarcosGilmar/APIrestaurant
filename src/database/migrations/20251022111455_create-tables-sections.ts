1eb2e85af00198285c0b91b2ed82d19f62031bba
marcosgilmar <marcosgilmarvalentini@gmail.com>
2025-10-22 11:07:05 -0300

    feat: add migration to create table "orders" and create controller related to it

--- src/database/migrations/20251022111455_create-tables-sections.ts
+++ src/database/migrations/20251022111455_create-tables-sections.ts
@@ -1,6 +1,5 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("table_sections", (table) => {
         table.increments("id").primary(),
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { productsRouter } from "./products-routes";
 import { tablesRouter } from "./tables-routes";
 import { tablesSectionsRouter } from "./tables-sections-routes";
+import { ordersRouter } from "./order-routes";
 
 const routes = Router()
 routes.use("/products", productsRouter)
 routes.use("/tables", tablesRouter)
 routes.use("/table_sections", tablesSectionsRouter)
+routes.use("/orders", ordersRouter)
 
 export { routes }
 
